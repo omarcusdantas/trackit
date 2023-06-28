0fbc04552d29265222229add9a5f9239b4985995
MarcusDantas <marvinsd@gmail.com>
2023-06-28 16:37:25 -0300

    feat: add Login page with connection do Driven's API and local storage to handle login persistance

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,89 @@
+import { Link, useNavigate } from "react-router-dom";
+import { LoginContainer } from "../styles/template";
+import logo from "../assets/logo.png";
+import axios from "axios";
+import React, { useRef, useContext, useEffect, useState } from "react";
+import { ThreeDots } from "react-loader-spinner";
+import { UserContext } from "../UserContext";
+
+export default function LoginPage() {
+    const inputEmailRef = useRef(null);
+    const inputPasswordRef = useRef(null);
+    const persistenceRef = useRef(null);
+    const [isDisabled, setIsDisabled] = useState(false);
+    const { setUserData } = useContext(UserContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem("user");
+
+        if (storedUser) {
+            const user = JSON.parse(storedUser);
+            setUserData(user);
+            navigate("/today");
+        }
+    }, []);
+
+    function handleForm(event) {
+        event.preventDefault();
+        setIsDisabled(true);
+
+        const data = {
+            email: inputEmailRef.current.value,
+            password: inputPasswordRef.current.value,
+        };
+
+        axios
+            .post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login", data)
+            .then((response) => {
+                setIsDisabled(false);
+                const newUserData = {
+                    token: response.data.token,
+                    progress: 0,
+                };
+                setUserData(newUserData);
+
+                if (persistenceRef.current.checked) {
+                    localStorage.setItem("user", JSON.stringify(newUserData));
+                }
+
+                navigate("/today");
+            })
+            .catch(() => {
+                setIsDisabled(false);
+                alert("The email or password is incorrect. Please try again.");
+            });
+    }
+
+    return (
+        <LoginContainer>
+            <img src={logo} alt="TrackIt" />
+            <form onSubmit={handleForm}>
+                <input
+                    type="email"
+                    placeholder="email"
+                    required
+                    ref={inputEmailRef}
+                    disabled={isDisabled}
+                />
+                <input
+                    type="password"
+                    placeholder="password"
+                    required
+                    ref={inputPasswordRef}
+                    disabled={isDisabled}
+                />
+                <label>
+                    <input type="checkbox" ref={persistenceRef} disabled={isDisabled} />
+                    Remember me
+                </label>
+                <button type="submit" disabled={isDisabled}>
+                    {isDisabled ? <ThreeDots height="13px" color="#ffffff" /> : "Login"}
+                </button>
+                <Link to="/sign-up">
+                    Don't have an account? Sign up!
+                </Link>
+            </form>
+        </LoginContainer>
+    );
+}
---
+++ src/styles/template.js
@@ -0,0 +1,145 @@
+import styled from "styled-components";
+
+export const LoginContainer = styled.div`
+    height: 667px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    img {
+        margin: 68px 0 33px 0;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 6px;
+        height: 300px;
+        align-items: center;
+    }
+
+    input {
+        width: 303px;
+        height: 45px;
+        border: 1px solid #D5D5D5;
+        border-radius: 5px;
+        font-size: 20px;
+        padding: 0 11px;
+
+        &::placeholder {
+            color: #DBDBDB;
+        }
+    }
+
+    button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #52B6FF;
+        color: #ffffff;
+        font-size: 20px;
+        width: 303px;
+        height: 45px;
+        border: none;
+        border-radius: 4.6px;
+        transition: all 0.3s ease;
+
+        &:disabled {
+            background-color: #86CCFF;
+            cursor: default;
+        }
+
+        &:not(:disabled):hover {
+            background-color: #5470FF;
+        }
+    }
+
+    a {
+        margin-top: 25px;
+        font-size: 14px;
+        color: #52B6FF;
+    }
+
+    label {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 20px;
+        margin-bottom: 10px;
+        border: 1px solid #D5D5D5;
+        color: #7A7A7A;
+        border-radius: 4.6px;
+        padding: 5px;
+    }
+
+    input[type="checkbox"] {
+        width: 23px;
+        height: 23px;
+    }
+`;
+
+export const PageContainer = styled.div`
+    background-color: #F2F2F2;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+`;
+
+export const Main = styled.div`
+    width: 375px;
+    margin-top: 92px;
+    margin-bottom: 115px;
+    padding: 0 18px;
+`;
+
+export const Title = styled.div`
+    min-height: 35px;
+    display: flex;
+    justify-content: space-between;
+    align-items: end;
+
+    h2 {
+        font-size: 23px;
+        color: #126BA5;
+    }
+
+    button {
+        width: 40px;
+        height: 35px;
+        background-color: #52B6FF;
+        border-radius: 4.6px;
+        position: relative;
+
+        p {
+            color: #ffffff;
+            font-size: 27px;
+            position: absolute;
+            top: -2px;
+            left: 12.7px;
+        }
+
+        ion-icon {
+            position: absolute;
+            color: #ffffff;
+            font-size: 30px;
+            top: 2px;
+            left: 5px;
+        }
+    }
+
+`;
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    margin-top: 29px;
+
+    p {
+        font-size: 18px;
+        color: #666666;
+        line-height: 22px;
+    }
+`;
+
